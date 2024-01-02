adc419575322e32fc4b7c76d79441ad6c05a97ec
Jackson Silva <cajui54@gmail.com>
2024-01-01 21:27:57 -0300

    fixing api

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+const cors = require('cors');
+
+
+
 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(cors({
+    origin: '*'
+    }))
 
 server.use (
     jsonServer.rewriter({
